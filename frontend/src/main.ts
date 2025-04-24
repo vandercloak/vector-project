c7fc8ef8db187d9150dc8ebc1111394f12bf47cf
Nick Vander Kolk <nicholas.vanderkolk@gmail.com>
2025-04-24 14:33:23 -0400

    Adding analytics to client

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
 
 app.use(router)
+
+// Initialize Vercel Analytics
+inject()
 
 app.mount('#app')
